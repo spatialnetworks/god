36d5ce4febee954c4b26162a1ffa37c94ab81a93
Zac McCormick <zac.mccormick@gmail.com>
2015-02-06 10:38:36 -0500

    Give god a name

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,5 @@
-maintainer        "Opscode, Inc."
+name              "god"
+aintainer         "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs and configures god and provides a define for monitoring"
